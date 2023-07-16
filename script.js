4cec6097c9df7cc0f91827c343f1cf83f1d36e0f
abbyjo <abbyjo225@gmail.com>
2023-07-16 19:43:01 -0400

    Added alert for invalid length

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Declarations
+//Object containing pw options
 var criteria = {
   lowerCase: "abcdefghijklmnopqrstuvwxyz",
   upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   numeric: "0123456789",
   symbols: "!@#$%^&*()_+~\`|}{[]:;?><,./-=",
-}
-//Functions to randomize selection of characters 
+} 
+//Array containing randomization functions
 var critRandom = [
   function lower() {return criteria.lowerCase[Math.floor(Math.random() * criteria.lowerCase.length)]},
   function upper() {return criteria.upperCase[Math.floor(Math.random() * criteria.upperCase.length)]},
   function number() {return criteria.numeric[Math.floor(Math.random() * criteria.numeric.length)]},
   function symbol() {return criteria.symbols[Math.floor(Math.random() * criteria.symbols.length)]}
 ]
+//Function to generate password 
 function generatePassword () {
-//User input 
-let pwLengthStr = prompt ("Set password length! Enter a number between 8 and 128~", "8");
-let pwLower = confirm ("Would you like to include lowercase characters? \nPress OK for Yes, Cancel for No");
-let pwUpper = confirm ("Would you like to include uppercase characters? \nPress OK for Yes, Cancel for No");
-let pwNum = confirm ("Would you like to include any numbers? \nPress OK for Yes, Cancel for No");
-let pwSpecial = confirm ("Would you like to include special characters? \nPress OK for Yes, Cancel for No");
-let pwLength = Number(pwLengthStr);
-if (pwLower + pwUpper + pwNum + pwSpecial === 0) {
-  alert("No criteria selected. Please select at least one!")
-}
-
-  (console.log("placeholder!"))
+//Series of prompts for user input
+  let pwLengthStr = prompt ("Set password length! Enter a number between 8 and 128~", "8");
+  let pwLength = Number(pwLengthStr);
+    if (pwLength<8 || pwLength >128) {
+      alert ("Length must be between 8 and 128! Please try again");
+      return;
+    }
+  let pwLower = confirm ("Would you like to include lowercase characters? \nPress OK for Yes, Cancel for No");
+  let pwUpper = confirm ("Would you like to include uppercase characters? \nPress OK for Yes, Cancel for No");
+  let pwNum = confirm ("Would you like to include any numbers? \nPress OK for Yes, Cancel for No");
+  let pwSpecial = confirm ("Would you like to include special characters? \nPress OK for Yes, Cancel for No");
+    if (pwLower + pwUpper + pwNum + pwSpecial === 0) {
+      alert("No criteria selected. Please select at least one!")
+    }
+    return;
 };
 
 // Write password to the #password input
