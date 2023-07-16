d2d7c263fefe3ced3eab96b1ed4e0f90b8ebe5a3
abbyjo <abbyjo225@gmail.com>
2023-07-16 13:31:14 -0400

    Added alert in event no criteria is selected

--- script.js
+++ script.js
@@ -12,12 +12,17 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  function generatePassword () {(console.log("placeholder!"))};
-  let pwLength = prompt ("Set password length! Enter a number between 8 and 128~", "8");
+  //User input 
+  let pwLengthStr = prompt ("Set password length! Enter a number between 8 and 128~", "8");
   let pwLower = confirm ("Would you like to include lowercase characters? \nPress OK for Yes, Cancel for No");
   let pwUpper = confirm ("Would you like to include uppercase characters? \nPress OK for Yes, Cancel for No");
   let pwNum = confirm ("Would you like to include any numbers? \nPress OK for Yes, Cancel for No");
   let pwSpecial = confirm ("Would you like to include special characters? \nPress OK for Yes, Cancel for No");
+  let pwLength = Number(pwLengthStr);
+  if (pwLower + pwUpper + pwNum + pwSpecial === 0) {
+    alert("No criteria selected. Please select at least one!")
+  }
+  function generatePassword () {(console.log("placeholder!"))};
 }
 
 // Add event listener to generate button
